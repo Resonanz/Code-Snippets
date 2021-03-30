802a39d63579826efafdddf7e53eff2bbde943f8
Alpha Particle <Resonanz@users.noreply.github.com>
2021-03-31 12:38:15 +1300

    Minor update

--- Windows venv creator.bat
+++ Windows venv creator.bat
@@ -2,7 +2,7 @@ echo OFF
 echo Windows venv creator will create a Python virtual environment (venv) and install a list of Python packages in that venv. After each package is installed, the installer will pause awaiting a keypress. This will enable you to check the install details as it progresses.
 
 First, install Python (suggested location is C:\)
-Second, make sure you are in the root of the folder you want to work from. 
+Second, make sure you are in the root of the folder you want to work from. Copy this batch file into that folder.
 
 pause
 
